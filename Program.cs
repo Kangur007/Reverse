b32abf3b207a56c8839add73284bd2ed8d0f452c
Łukasz Wróbel <30751704+Kangur007@users.noreply.github.com>
2022-01-11 21:42:32 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Zabawa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string s = "Ala ma kota.";
+
+
+            Console.WriteLine($"Reverse string: {ReverseWordsInSentence(s)}");
+        }
+
+
+        static string ReverseWordsInSentence(string s)
+        {
+
+            s = s.Remove(s.Length - 1);
+            string[] words = s.Split(' ');
+            Array.Reverse(words);
+            return string.Join(" ", words) + ".";
+
+
+
+        }
+
+    }
+
+}
